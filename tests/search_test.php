ba7a0f38a452649c3499789cb7c7beb2d1b09257
NeillM <neill.magill@nottingham.ac.uk>
2014-03-18 11:28:20 +0000

    Unit test for the block_courseimport_search class

---
+++ tests/search_test.php
@@ -0,0 +1,86 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Tests the block_courseimport_search class.
+ *
+ * @package     block_courseimport
+ * @copyright   University of Nottingham, 2014
+ * @author      Neill Magill <neill.magill@nottingham.ac.uk>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @group block_courseimport
+ * @group uon
+ */
+class block_courseimport_search_testcase extends advanced_testcase {
+    /**
+     * Tests that the block_courseimport_search::searchshortname method works correctly.
+     *
+     * @covers block_courseimport_search::searchshortname
+     * @covers block_courseimport_search::get_shortnameresults
+     * @group block_courseimport
+     * @group uon
+     */
+    public function test_searchshortname() {
+        $this->resetAfterTest(true);
+
+        /* First create some courses. */
+        // This group should all match each other.
+        $course0g1 = self::getDataGenerator()->create_course(array('shortname' => 'XX3L11-UK-FYR1112'));
+        $course1g1 = self::getDataGenerator()->create_course(array('shortname' => 'XX3L11-UK-FYR1213'));
+        $course2g1 = self::getDataGenerator()->create_course(array('shortname' => 'XX3L11-UK-FYR1314'));
+        $course3g1 = self::getDataGenerator()->create_course(array('shortname' => 'XX3L11-MY-FYR1314'));
+        $course4g1 = self::getDataGenerator()->create_course(array('shortname' => 'XX3L11-CN-FYR1314'));
+        // The UK codes in this group group should also match each other.
+        $course0g2 = self::getDataGenerator()->create_course(array('shortname' => 'LE-CAREERS-ECON-UK-1213'));
+        $course1g2 = self::getDataGenerator()->create_course(array('shortname' => 'LE-CAREERS-ECON-UK-1314'));
+        $course2g2 = self::getDataGenerator()->create_course(array('shortname' => 'LE-CAREERS-ECON-CN-1314'));
+        // Some random courses.
+        $course0g3 = self::getDataGenerator()->create_course(array('shortname' => 'XX3L12-UK-FYR1413'));
+        $course0g3 = self::getDataGenerator()->create_course(array('shortname' => 'G53NMD-UK-AUT-G53NMD-MY-AUT-1314'));
+        $course0g3 = self::getDataGenerator()->create_course(array('shortname' => 'PA-HS-INDUCT-UK'));
+
+        $search = new block_courseimport_search();
+
+        $results0 = $search->searchshortname('XX3L11', 'XX3L11-UK-FYR1112');
+        $this->assertEquals(4, $results0);
+        // Check that caching is working correctly.
+        $results1 = $search->searchshortname('ggg', '');
+        $this->assertNotEquals($results1, $results0);
+
+        $search = new block_courseimport_search();
+        $results0 = $search->get_shortnameresults('XX3L11', 'XX3L11-UK-FYR1112');
+        $this->assertArrayHasKey($course1g1->id, $results0);
+        $this->assertArrayHasKey($course2g1->id, $results0);
+        $this->assertArrayHasKey($course3g1->id, $results0);
+        $this->assertArrayHasKey($course4g1->id, $results0);
+        // Check caching.
+        $results1 = $search->get_shortnameresults('ggg', '');
+        $results2 = $search->get_shortnameresults('XX3L11', 'XX3L11-UK-FYR1112');
+        $this->assertNotEquals($results1, $results0);
+        $this->assertEquals($results2, $results0);
+
+        $results0 = $search->get_shortnameresults('LE-CAREERS-ECON-UK-', 'LE-CAREERS-ECON-CN-1314');
+        $this->assertArrayHasKey($course0g2->id, $results0);
+        $this->assertArrayHasKey($course1g2->id, $results0);
+        $this->assertCount(2, $results0);
+        $results0 = $search->searchshortname('LE-CAREERS-ECON-UK-', 'LE-CAREERS-ECON-CN-1314');
+        $this->assertEquals(2, $results0);
+
+        $this->assertDebuggingNotCalled();
+    }
+}
