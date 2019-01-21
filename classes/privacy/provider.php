7491e7dfdc7113c3b2c685928e7d067443fce7e5
Yijun Xue <xuey@hotmail.com>
2019-01-21 14:58:29 +0000

    add tab to implements lines

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -39,9 +39,9 @@ require_once($CFG->dirroot . '/blocks/courseimport/lib.php');
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class provider implements
-    \core_privacy\local\metadata\provider,
-    \core_privacy\local\request\core_userlist_provider,
-    \core_privacy\local\request\plugin\provider {
+        \core_privacy\local\metadata\provider,
+        \core_privacy\local\request\core_userlist_provider,
+        \core_privacy\local\request\plugin\provider {
 
     /**
      * Returns meta data about this system.
