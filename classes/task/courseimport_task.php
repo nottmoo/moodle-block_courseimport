2329310639aee2544ab06f44980972f086bb031c
Neill Magill <neill.magill@nottingham.ac.uk>
2024-10-15 14:57:01 +0100

    MDLE-2448 Code style: Lines that are too long

--- classes/task/courseimport_task.php
+++ classes/task/courseimport_task.php
@@ -121,7 +121,14 @@ class courseimport_task extends \core\task\scheduled_task {
             throw new job_failed($message);
         }
 
-        $rc = new \restore_controller($job->bid, $job->target, \backup::INTERACTIVE_YES, \backup::MODE_IMPORT, $job->user, \backup::TARGET_CURRENT_ADDING);
+        $rc = new \restore_controller(
+            $job->bid,
+            $job->target,
+            \backup::INTERACTIVE_YES,
+            \backup::MODE_IMPORT,
+            $job->user,
+            \backup::TARGET_CURRENT_ADDING
+        );
         $rc->set_progress(new \core\progress\db_updater($job->id, 'block_courseimport', 'restoreprogress'));
         $this->prepare_for_restore($rc, $job);
 
